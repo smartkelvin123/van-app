47011a1ce9d3f64fbf21a025bb83bdd703eb18c3
unknown <smartkelvin66@gmail.com>
2023-07-29 15:07:06 +0100

    sad path

--- src/pages/Host/Login.jsx
+++ src/pages/Host/Login.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import { loginUser } from "../../Api";
 
 export function loader({ request }) {
   return new URL(request.url).searchParams.get("message");
 }
 
 export default function Login() {
-  const [loginFormData, setLoginFormData] = React.useState({
+  const [loginFormData, setLoginFormData] = useState({
     email: "",
     password: "",
   });
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
   const message = useLoaderData();
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(loginFormData);
+    setStatus("submitting");
+    setError(null);
+    loginUser(loginFormData)
+      .then((data) => console.log(data))
+      .catch((err) => setError(err))
+      .finally(() => setStatus("idle"));
   }
 
   function handleChange(e) {
@@ -28,7 +36,9 @@ export default function Login() {
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
+
       {message && <h3 className="red">{message}</h3>}
+      {error && <h3 className="red">{error.message}</h3>}
       <form onSubmit={handleSubmit} className="login-form">
         <input
           name="email"
@@ -44,7 +54,9 @@ export default function Login() {
           placeholder="Password"
           value={loginFormData.password}
         />
-        <button>Log in</button>
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}
+        </button>
       </form>
     </div>
   );
